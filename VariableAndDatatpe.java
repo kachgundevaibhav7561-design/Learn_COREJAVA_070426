aaa47c4d6a632e1c0b478e4110224362454e5d87
kachgundevaibhav7561-design <kachgundevaibhav7561@gmail.com>
2026-04-08 20:39:22 +0530

    start

---
+++ VariableAndDatatpe.java
@@ -0,0 +1,50 @@
+public class VariableAndDatatpe {
+
+    public static void main(String[] args) {
+        // bit change the byte
+        byte a = 1;
+        short b = 2;
+        int c = 10;
+        long d = 12021;
+        boolean f = true;
+        double s = 45.20;
+        char w = 'c';
+        float x = 34.5f;
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(f);
+        System.out.println(s);
+        System.out.println(w);
+        System.out.println(x);
+
+
+        int num1=12;
+        int num2=34;
+     
+    /*Operators:
+            arthimetic :-
+            +,-,*,/.%
+            used in mathematical operations,
+    
+
+            Realational opertator:
+            <=,>=,<,>,==,!=
+            relational operators always retuen boolean value .
+               System.out.println(num1>num2); false 
+               System.out.println(num1<num2); true
+
+           Logigical operators:
+                &&,||,!
+               
+
+
+        
+    */
+
+
+
+    }
+}
